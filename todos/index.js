c3a542bd17d8da6651851ee5f9cfc23719b0e178
Kirk <kirkdmcm@gmail.com>
2017-06-23 16:26:17 -0600

    todos second commit

--- todos/index.js
+++ todos/index.js
@@ -1,8 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow();
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+var menuTemplate = [
+    {
+        label: 'file'
+    }
+];
